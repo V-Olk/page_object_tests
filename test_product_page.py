fefc95bad4d6eb76f3d442a167965ae18c497396
Vitamincar <volkin.tflex@gmail.com>
2021-05-05 14:55:05 +0300

    transfer login tests from main page to base

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,12 @@
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
+from .pages.login_page import LoginPage
 import pytest
 
 #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 @pytest.mark.parametrize('offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 @pytest.mark.skip
@@ -28,6 +29,7 @@ def test_guest_can_add_product_to_basket(browser, offer):
     product_page.should_be_the_same_name_of_product(product_name)
     product_page.should_be_the_same_price_of_product(product_price)
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     page = MainPage(browser, link)
@@ -40,6 +42,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     product_page.should_not_be_success_message()
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     page = MainPage(browser, link)
     page.open()  
@@ -48,6 +51,7 @@ def test_guest_cant_see_success_message(browser):
     #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     product_page.should_not_be_success_message()
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     page = MainPage(browser, link)
@@ -59,3 +63,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
     # Проверяем, что нет сообщения об успехе с помощью is_disappeared
     product_page.should_dissapear_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
